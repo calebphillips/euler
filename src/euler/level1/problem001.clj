59945fce730bdd148f8914d0b88858aae9c8ccb6
calebphillips <caleb.phillips@gmail.com>
2011-04-15 12:14:10 -0500

    Updated #1 to one liner

--- src/euler/level1/problem001.clj
+++ src/euler/level1/problem001.clj
@@ -7,9 +7,5 @@
   (fn [n] (some #(divides? n %) divisors)))  
 
 (defn euler-1 [upper-bound]
-  (let [divisible? (divides-any? 3 5)]
-    (loop [sum 0 n 1]
-      (if (= upper-bound n)
-        sum
-        (recur (if (divisible? n) (+ sum n) sum) (inc n)))))
-  )
+  (reduce + (filter (divides-any? 3 5) (range 1 upper-bound))))
+
