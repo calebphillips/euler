8f0f0ce5a3060b011b825bf56d2d8a8b988538a2
calebphillips <caleb.phillips@gmail.com>
2011-04-15 11:56:00 -0500

    Finished recursive soln to #1

--- spec/euler/level1/problem001_spec.clj
+++ spec/euler/level1/problem001_spec.clj
@@ -5,11 +5,26 @@
 
 ; http://projecteuler.net/index.php?section=problems&id=1
 
-(describe "Euler Problem #1"
+(describe "divides?"
+    (it "works"
+        (should (divides? 9 3))
+        (should (divides? 333 3))
+        (should-not (divides? 4 3))
+        (should (divides? 25 5))
+        (should-not (divides? 973 5))))
 
+(describe "divides-any?"
+          (it "works"
+              (should ((divides-any? 3 5) 9))
+              (should ((divides-any? 3 5) 10))
+              (should-not ((divides-any? 3 5) 16))))
+
+
+(describe "Euler Problem #1"
   (it "Solves #1"
     (should= 23 (euler-1 10))
-	(should= -1 (euler-1 1000)))
+    (should= 78 (euler-1 20))
+    (should= 233168 (euler-1 1000)))
   )
 
 (run-specs)
--- src/euler/level1/problem001.clj
+++ src/euler/level1/problem001.clj
@@ -1,4 +1,15 @@
 (ns euler.level1.problem001)
 
-(defn euler-1 [n]
+(defn divides? [dividend divisor] 
+  (zero? (rem dividend divisor)))
+
+(defn divides-any? [& divisors]
+  (fn [n] (some #(divides? n %) divisors)))  
+
+(defn euler-1 [upper-bound]
+  (let [divisible? (divides-any? 3 5)]
+    (loop [sum 0 n 1]
+      (if (= upper-bound n)
+        sum
+        (recur (if (divisible? n) (+ sum n) sum) (inc n)))))
   )
