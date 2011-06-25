1821bbd5a04bf13440f3a6e61019f9693a790c30
calebphillips <caleb.phillips@gmail.com>
2011-06-24 22:03:49 -0500

    #8

---
+++ spec/euler/level1/problem008_spec.clj
@@ -0,0 +1,15 @@
+(ns euler.level1.problem008-spec
+  (:use
+    [speclj.core]
+    [euler.level1.problem008]))
+
+; http://projecteuler.net/index.php?section=problems&id=8
+
+(describe "Euler Problem #8"
+
+  (it "Solves #8"
+    ; (should= -1 (euler-8))
+      )
+  )
+
+(run-specs)
