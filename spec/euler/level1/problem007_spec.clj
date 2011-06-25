4735cc30e101f497ab9713f31baa669ccd83a60b
calebphillips <caleb.phillips@gmail.com>
2011-06-24 21:40:23 -0500

    #7

--- spec/euler/level1/problem007_spec.clj
+++ spec/euler/level1/problem007_spec.clj
@@ -8,7 +8,7 @@
 (describe "Euler Problem #7"
 
   (it "Solves #7"
-    (should= -1 (euler-7 -1)))
+    (should= 13 (euler-7 6)))
   )
 
 (run-specs)
--- src/euler/level1/problem007.clj
+++ src/euler/level1/problem007.clj
@@ -1,4 +1,7 @@
-(ns euler.level1.problem007)
+(ns euler.level1.problem007
+  (:use [euler.common :only [prime?]]))
 
 (defn euler-7 [n]
-  )
+  (first 
+    (drop (dec n) 
+          (filter prime? (iterate inc 2)))))
