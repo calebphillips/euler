82026b3c0e83ad8a896b011f1e72bbd62ef6bee8
calebphillips <caleb.phillips@gmail.com>
2011-06-27 14:33:37 -0500

    Little more tinkerin' with #11

--- src/euler/level1/problem011.clj
+++ src/euler/level1/problem011.clj
@@ -10,22 +10,24 @@
 (defn apply-deltas-to-point [deltas point]
   (map #(map + point %) deltas))
 
-(defn find-adj [deltas grid & point]
+(defn find-adj-values [deltas grid & point]
   (let [size (sq-size grid)
         valid-pt (fn [p] (every? #(< -1 % size) p))]
     (map #(apply from-coords grid %)
-         (filter valid-pt 
-                 (apply-deltas-to-point deltas point)))))
+         (filter valid-pt
+                 (conj (apply-deltas-to-point deltas point) 
+                       point)))))
 
-(def horizontal (partial find-adj [[0 0] [1 0] [2 0] [3 0]]))
-(def vertical   (partial find-adj [[0 0] [0 1] [0 2] [0 3]]))
-(def l-to-r     (partial find-adj [[0 0] [1 1] [2 2] [3 3]]))
-(def r-to-l     (partial find-adj [[0 0] [-1 1] [-2 2] [-3 3]]))
+(def horizontal (partial find-adj-values [[1 0] [2 0] [3 0]]))
+(def vertical   (partial find-adj-values [[0 1] [0 2] [0 3]]))
+(def l-to-r     (partial find-adj-values [[1 1] [2 2] [3 3]]))
+(def r-to-l     (partial find-adj-values [[-1 1] [-2 2] [-3 3]]))
 
 (defn euler-11 [grid]
   (apply max
          (map #(reduce * %)
               (filter #(= 4 (count %))
-                      (mapcat #(for [x (range 0 20) y (range 0 20)] (% grid x y))
-                              [horizontal vertical l-to-r r-to-l])))))
+                      (let [r (range 0 (sq-size grid))]
+                        (mapcat #(for [x r y r] (% grid x y))
+                                [horizontal vertical l-to-r r-to-l]))))))
 
