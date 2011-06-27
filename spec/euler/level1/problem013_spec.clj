0450bc675279eb3ae0db8f301fd276635bb65332
calebphillips <caleb.phillips@gmail.com>
2011-06-27 06:49:19 -0500

    #13

---
+++ spec/euler/level1/problem013_spec.clj
@@ -0,0 +1,14 @@
+(ns euler.level1.problem013-spec
+  (:use
+    [speclj.core]
+    [euler.level1.problem013]))
+
+; http://projecteuler.net/index.php?section=problems&id=13
+
+(describe "Euler Problem #13"
+
+  (it "Solves #13"
+    )
+  )
+
+(run-specs)
