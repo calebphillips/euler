16eac34567276fc48c57fb1382915e70a94f13e0
calebphillips <caleb.phillips@gmail.com>
2011-06-28 16:41:51 -0500

    Clean up on isle #14

--- src/euler/level1/problem014.clj
+++ src/euler/level1/problem014.clj
@@ -2,14 +2,17 @@
 
 (defn the-seq [n]
   (loop [n n s []]
-    (if (= n 1)
-      (conj s 1)
-      (recur (if (even? n)
-               (/ n 2)
-               (+ 1 (* 3 n))) (conj s n))))) 
+    (let [s (conj s n)
+          next-term #(if (even? n)
+                       (/ n 2)
+                       (+ 1 (* 3 n)))]
+      (if (= n 1)
+        s
+        (recur (next-term) s)))))
 
 (defn euler-14 [n]
   (first
     (reduce (fn [a b] (if (> (second b) (second a)) b a)) 
-            (for [x (range 1 n)] [x (count (the-seq x))])))
+            (for [x (range 1 n)] 
+              [x (count (the-seq x))])))
   )
